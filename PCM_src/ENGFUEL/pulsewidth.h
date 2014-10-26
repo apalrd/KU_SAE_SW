7ec3925b63e96e7899046324113fd5ee2b350231
Andrew <apalrd@gmail.com>
2014-10-26 14:59:51 -0400

    Worked on pulse width interfaces

--- PCM_src/ENGFUEL/pulsewidth.h
+++ PCM_src/ENGFUEL/pulsewidth.h
@@ -5,15 +5,60 @@
 
 
 //Variables part of Pulsewidth
-//Total working pulse width for primaries
+//Total working pulse width by injector #
 extern volatile float FUEL_PWTotalWork[];
-//Total working pulse width for secondaries
-extern volatile float FUEL_PWTotalWorkSec[];
 //Drop Dead angle for primaries
 extern volatile float FUEL_PWAngDropDead[];
+//Pulse width requested at initial pulse event
+extern volatile float FUEL_PWInitial[];
+//Pulse width requested at make-up pulse
+extern volatile float FUEL_PWMakeUp[];
+//Pulse actually delivered, reported by eTPU
+extern volatile float FUEL_PWActual[];
+//Dead time, global
+extern volatile float FUEL_PWDead;
+//nonlinear time
+extern volatile float FUEL_PWNonlin;
+//Injector is operating in nonlinear zone flag
+extern volatile U8 FUEL_PWIsNonlin[];
+//Injection pressure per injector
+extern volatile float FUEL_PWInjPrs[];
+//Enum of which pressure to use for downstream pressure
+extern volatile U8 FUEL_PWPrsDown[];
+//Enum of which pressure to use for upstream pressure
+extern volatile U8 FUEL_PWPrsUp[];
+//Field of the pressures which can be used as downstream
+extern volatile float FUEL_PrsDown[];
+//Field of the pressures which can be used as upstream
+extern volatile float FUEL_PrsUp[];
+//Mass requested by injector #
+extern volatile float FUEL_PWDesMass[];
+//Mass actual by injector #
+extern volatile float FUEL_PWActMass[];
+//FUEL Puddle must report masses per injector
+//It is the responsibility of Puddle to
+//map cylinders/banks to injectors
 
 
 //Variables part of Pulsewidth - Calibrations
+//Injector flow surface from injection pressure and mass des
+//for injectors flagged as primary injectors
+extern volatile float FUEL_PWInjFlowSurfPrimSurf[];
+//Injector flow surface for secondary injectors
+extern volatile float FUEL_PWInjFlowSurfSecSurf[];
+//Nonlinear correction table for primaries
+extern volatile float FUEL_PWInjNonlinPrimSurf[];
+//For secondaries
+extern volatile float FUEL_PWInjNonlinSecSurf[];
+//Nonlinear threshold by injection pressure
+extern volatile float FUEL_PWNonlinThrPrimTbl[];
+//For secondaries
+extern volatile float FUEL_PWNonlinThrSecTbl[];
+//Battery voltage breakpoints for pulse width
+extern volatile float FUEL_PWBattBkp[];
+//Injection pressure breakpoints for pulse width
+extern volatile float FUEL_PWInjPreBkp[];
+
 
 
 
